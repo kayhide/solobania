efd434fd5f309c611577419c52c4194c5f00905d
Hideaki Kawai <kayhide@gmail.com>
2023-10-21 19:24:14 +0200

    Create Act#parent

--- rails/app/models/act.rb
+++ rails/app/models/act.rb
@@ -4,4 +4,19 @@ class Act < ApplicationRecord
 
   MARKS = %w(confident hesitant uncertain)
   enum mark: MARKS.map { |x| [x, x] }.to_h
+
+  def parent
+    @parent ||=
+      begin
+        refl =
+          actable.class.reflect_on_all_associations(:belongs_to).find {|refl|
+            refl.klass.reflect_on_association :acts
+          }
+        if refl
+          parent_actable = actable.association(refl.name).load_target
+          parent_actable.acts.first || parent_actable.acts.build(user: user)
+        end
+      end
+    end
+  end
 end
--- rails/spec/models/act_spec.rb
+++ rails/spec/models/act_spec.rb
@@ -1,4 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Act, type: :model do
+  describe "#parent" do
+    context "of problem" do
+      let(:problem) { create :problem }
+      subject { build :act, actable: problem }
+
+      it "builds sheet act" do
+        parent = subject.parent
+        expect(parent.actable).to eq problem.sheet
+      end
+
+      it "returns sheet act" do
+        sheet_act = create :act, actable: problem.sheet
+        parent = subject.parent
+        expect(parent).to eq sheet_act
+        expect(parent.actable).to eq problem.sheet
+      end
+    end
+
+    context "of sheet" do
+      let(:sheet) { create :sheet }
+      subject { build :act, actable: sheet }
+
+      it "builds pack act" do
+        parent = subject.parent
+        expect(parent.actable).to eq sheet.pack
+      end
+
+      it "returns pack act" do
+        pack_act = create :act, actable: sheet.pack
+        parent = subject.parent
+        expect(parent).to eq pack_act
+        expect(parent.actable).to eq sheet.pack
+      end
+    end
+  end
 end
